1f6d6b4a865ab750f0c663b7f1667c3a3aa8980a
SiriusXT <1160925501@qq.com>
2023-06-08 14:37:59 +0800

    0.5

--- Dark-mode-img-color-conversion.js
+++ Dark-mode-img-color-conversion.js
@@ -1,7 +1,7 @@
 /*
 Blue theme
 https://github.com/SiriusXT/trilium-theme-blue
-Dark-mode-img-color-conversion.js version:0.3 for trilium:>0.58.4
+Dark-mode-img-color-conversion.js version:0.5 for trilium:>0.58.4
 */
 var themeStyle = getComputedStyle(document.documentElement).getPropertyValue('--theme-style');
 // Get the value of the CSS custom property "--theme-style" of the document element and assign it to the variable "themeStyle"
@@ -13,7 +13,7 @@ function colorTrans() {
     if (!$(this).hasClass('color-trans')) {
       $(this).addClass('color-trans');
       var bgColor = $(this).css('background-color');
-      var rgbaColor = bgColor.replace(')', ', 0.3)').replace('rgb', 'rgba');
+      var rgbaColor = bgColor.replace(')', ', 0.2)').replace('rgb', 'rgba');
       $(this).css('background-color', rgbaColor);
     }
   });
@@ -29,43 +29,53 @@ function imgColorInversion() {
     // Iterate over all <img> elements in the document and skip the iteration if the element already has either "imgInversion" or "imgNotInversion" class
     var imgObj = new Image();
     // Create a new Image object to get the pixel information
+    imgObj.src = img.src;
 
     imgObj.onload = function () {
-      // When the image is loaded, get its pixel data
-      var canvas = document.createElement('canvas');
-      canvas.width = imgObj.width;
-      canvas.height = imgObj.height;
-      var ctx = canvas.getContext('2d');
-      ctx.drawImage(imgObj, 0, 0);
-      // Get the pixel data from the canvas
-      var imageData = ctx.getImageData(0, canvas.height * 2 / 3, canvas.width / 3, canvas.height / 3).data;
-      var rSum = 0, gSum = 0, bSum = 0;
+      try {
+        // When the image is loaded, get its pixel data
+        var canvas = document.createElement('canvas');
+        canvas.width = imgObj.width;
+        canvas.height = imgObj.height;
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(imgObj, 0, 0);
+        // Get the pixel data from the canvas
+        var imageData = ctx.getImageData(0, 0, canvas.width / 2, canvas.height / 2).data;
+        var rSum = 0, gSum = 0, bSum = 0;
 
-      for (var i = 0; i < imageData.length; i += 4) {
-        rSum += imageData[i];
-        gSum += imageData[i + 1];
-        bSum += imageData[i + 2];
-      }
-
-      // Calculate the average pixel color
-      var pixelCount = imageData.length / 4;
-      var rAvg = Math.round(rSum / pixelCount);
-      var gAvg = Math.round(gSum / pixelCount);
-      var bAvg = Math.round(bSum / pixelCount);
-      // Check if the background is white
-      if ((themeStyle.indexOf('dark') > 0 && ((rAvg > 200) + (gAvg > 200) + (bAvg > 200) >= 2)) || (themeStyle.indexOf('light') > 0 && ((rAvg < 50) + (gAvg < 50) + (bAvg < 50) >= 2))) {
-        if (!$(img).hasClass('imgInversion')) {
-          $(img).addClass('imgInversion');
+        for (var i = 0; i < imageData.length; i += 4) {
+          rSum += imageData[i];
+          gSum += imageData[i + 1];
+          bSum += imageData[i + 2];
         }
-      }
-      else {
+
+        // Calculate the average pixel color
+        var pixelCount = imageData.length / 4;
+        var rAvg = Math.round(rSum / pixelCount);
+        var gAvg = Math.round(gSum / pixelCount);
+        var bAvg = Math.round(bSum / pixelCount);
+        //console.log(pixelCount,rAvg,gAvg,bAvg);
+        // Check if the background is white
+        if ((themeStyle.indexOf('dark') >= 0 && ((rAvg > 200) + (gAvg > 200) + (bAvg > 200) >= 2)) || (themeStyle.indexOf('light') >= 0 && ((rAvg < 50) + (gAvg < 50) + (bAvg < 50) >= 2))) {
+          if (!$(img).hasClass('imgInversion')) {
+            $(img).addClass('imgInversion');
+          }
+        }
+        else {
+          if (!$(img).hasClass('imgNotInversion')) {
+            $(img).addClass('imgNotInversion');
+          }
+        }
+      } catch (error) {
+        $(img).removeClass('imgInversion');
         if (!$(img).hasClass('imgNotInversion')) {
           $(img).addClass('imgNotInversion');
         }
       }
     }
 
-    imgObj.src = img.src;
+
+
   });
 }
 // Iterate over all <img> elements in the document and add the "imgInversion" or "imgNotInversion" class based on the background color of the image
@@ -118,8 +128,7 @@ class blackStyle extends api.NoteContextAwareWidget {
           }
           colorTrans();
           imgColorInversion();
-        }
-          , 200);
+        }, 200);
       });
     });
 
