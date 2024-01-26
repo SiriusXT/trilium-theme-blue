935fe450d2ce9f270a685f47e4882db52aa81827
SiriusXT <1160925501@qq.com>
2024-01-26 10:25:18 +0800

    0.8

--- Dark-mode-img-color-conversion.js
+++ Dark-mode-img-color-conversion.js
@@ -1,5 +1,5 @@
 /*
-Dark-mode-img-color-conversion.js version:0.7 for trilium:>0.58.4
+Dark-mode-img-color-conversion.js version:0.8 for trilium:>0.58.4
 https://github.com/SiriusXT/trilium-theme-blue/blob/main/Dark-mode-img-color-conversion.js.
 You can add the #disableAdjustImage tag to the note if you don’t want the color of a certain note image to be inverted.
 */
@@ -7,8 +7,47 @@ You can add the #disableAdjustImage tag to the note if you don’t want the colo
 let autoAdjustImage = true;
 let autoAdjustColor = true;
 
+//------------------------------------------------------------------------------------------------
 // Get the value of the CSS custom property "--theme-style" of the document element and assign it to the variable "themeStyle"
 var themeStyle = getComputedStyle(document.documentElement).getPropertyValue('--theme-style');
+var timeoutIds = [];
+function clearAllTimeouts() {
+  for (var i = 0; i < timeoutIds.length; i++) {
+    clearTimeout(timeoutIds[i]);
+  }
+  timeoutIds = []; 
+}
+
+function getTargetElement() {//async
+  return $("div.component.note-split:not(.hidden-ext) div.component.scrolling-container");
+  /*let maxAttempts = 1
+  let targetElement = null;
+  let attempts = 0;
+  
+  while (!targetElement && attempts < maxAttempts) {
+    try {
+      if (await this.noteContext.isReadOnly()) {
+          targetElement = await this.noteContext.getContentElement();
+      } else {
+        const textEditor = await this.noteContext.getTextEditor();
+        if (textEditor && textEditor.editing && textEditor.editing.view && textEditor.editing.view.domRoots) {
+          targetElement = $(textEditor.editing.view.domRoots.values().next().value);
+        }
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+    attempts++;
+    if (!targetElement) {
+      await new Promise(resolve => setTimeout(resolve, 1));
+    }
+  }
+  if (!targetElement) {
+    console.log("Tried maximum number of times but failed to obtain a valid targetElement.");
+  }
+  return targetElement;*/
+}
+
 
 class autoAdjustTheme extends api.NoteContextAwareWidget {
   get position() {
@@ -18,70 +57,69 @@ class autoAdjustTheme extends api.NoteContextAwareWidget {
   get parentWidget() {
     return 'center-pane';
   }
-
+  isEnabled() {
+    return super.isEnabled()
+      && this.note.type === 'text';
+  }
   doRender() {
     this.$widget = $(`<style type="text/css">
-.note-detail-editable-text img.imgInversion, .note-detail-readonly-text img.imgInversion {
-  filter: invert(88%) hue-rotate(180deg);
-}
-.note-detail-editable-text img:not(.imgInversion), .note-detail-editable-text  img:not(.imgNotInversion), .note-detail-readonly-text img:not(.imgInversion), .note-detail-readonly-text  img:not(.imgNotInversion) {
- visibility: hidden;
-}
-.note-detail-editable-text img.imgInversion, .note-detail-editable-text img.imgNotInversion, .note-detail-readonly-text img.imgInversion, .note-detail-readonly-text img.imgNotInversion{
-  visibility:visible;
-}
-.note-detail-editable-text img.imgNotInversionImportant, .note-detail-editable-text img.imgNotInversionImportant, .note-detail-readonly-text img.imgNotInversionImportant, .note-detail-readonly-text img.imgNotInversionImportant{
-  visibility:visible !important;
-  filter: invert(0) hue-rotate(0deg) !important;
-}
-</style>`);
+    .note-detail-editable-text img.imgInversion, .note-detail-readonly-text img.imgInversion {
+      filter: invert(88%) hue-rotate(180deg);
+    }
+    .note-detail-editable-text img:not(.imgInversion), .note-detail-editable-text  img:not(.imgNotInversion), .note-detail-readonly-text img:not(.imgInversion), .note-detail-readonly-text  img:not(.imgNotInversion) {
+       visibility: hidden;
+    }
+    .note-detail-editable-text img.imgInversion, .note-detail-editable-text img.imgNotInversion, .note-detail-readonly-text img.imgInversion, .note-detail-readonly-text img.imgNotInversion{
+       visibility:visible;
+    }
+    .note-detail-editable-text img.imgNotInversionImportant, .note-detail-editable-text img.imgNotInversionImportant, .note-detail-readonly-text img.imgNotInversionImportant, .note-detail-readonly-text img.imgNotInversionImportant{
+      visibility:visible !important;
+      filter: invert(0) hue-rotate(0deg) !important;
+    }
+    </style>`);
     return this.$widget;
   }
 
+
+
   async refreshWithNote(note) {
+    clearAllTimeouts();
+    var thisnote = this;
+    //console.log("Dark-mode-img-color-conversion");
     if (note.type != "text") { return; }
-    let targetElement = await this.getTargetElement();
-    let disableAdjustImage = false;
-    if (note.isLabelTruthy("disableAdjustImage")) {
-      disableAdjustImage = true;
-    }
-    if (autoAdjustImage) { this.adjustImage(targetElement, disableAdjustImage); };
-    if (autoAdjustColor) { this.adjustColor(targetElement); }
-  }
+    //thisnote=this;
 
-  async getTargetElement() {
-    let maxAttempts = 100
-    let targetElement = null;
-    let attempts = 0;
+    function Adjust() {
+      var targetElement = getTargetElement();//await
+      //window.aaa=targetElement;
+      let disableAdjustImage = false;
+      if (note.isLabelTruthy("disableAdjustImage")) {
+        disableAdjustImage = true;
+      }
+      if (autoAdjustImage) { thisnote.adjustImage(targetElement, disableAdjustImage); };
+      if (autoAdjustColor) { thisnote.adjustColor(targetElement); }
+    }
+    $(document).ready(function () {
+      Adjust();  
+      timeoutIds.push(setTimeout(Adjust, 200));
+      timeoutIds.push(setTimeout(Adjust, 500));
+      timeoutIds.push(setTimeout(Adjust, 1000));
+      timeoutIds.push(setTimeout(Adjust, 2000));
+      timeoutIds.push(setTimeout(Adjust, 4000));
+      //timeoutIds.push(setTimeout(Adjust, 6000));
+    });
 
-    while (!targetElement && attempts < maxAttempts) {
-      try {
-        if (await this.noteContext.isReadOnly()) {
-          targetElement = await this.noteContext.getContentElement();
-        } else {
-          const textEditor = await this.noteContext.getTextEditor();
-          if (textEditor && textEditor.editing && textEditor.editing.view && textEditor.editing.view.domRoots) {
-            targetElement = $(textEditor.editing.view.domRoots.values().next().value);
-          }
-        }
-      } catch (error) {
-        console.error("An error occurred:", error);
-      }
-      attempts++;
-      if (!targetElement) {
-        await new Promise(resolve => setTimeout(resolve, 50));
-      }
-    }
-    if (!targetElement) {
-      console.log("Tried maximum number of times but failed to obtain a valid targetElement.");
-    }
-    return targetElement;
   }
 
 
   async adjustImage(targetElement, disableAdjustImage) {
-    $(targetElement).find("img").each(function () {
+    //console.log("Dark-mode-img-color-conversion: adjustImage");
+    //window.targetElement = targetElement;
+    //var imgElements = $(targetElement).find("img").get().reverse();
+    //$.each(imgElements, function(index, imgElement) {
+    $(targetElement).find("img").each(function () {//
       var img = this;
+      //console.log(img);
       if (disableAdjustImage) {
         $(img).addClass('imgNotInversionImportant');
         return;
@@ -118,7 +156,7 @@ class autoAdjustTheme extends api.NoteContextAwareWidget {
           var bAvg = Math.round(bSum / pixelCount);
           //console.log(pixelCount,rAvg,gAvg,bAvg);
           // Check if the background is white
-          if (!disableAdjustImage && (themeStyle.indexOf('dark') >= 0 && ((rAvg > 200) + (gAvg > 200) + (bAvg > 200) >= 2))) { // || (themeStyle.indexOf('light') >= 0 && false)
+          if (!disableAdjustImage && (themeStyle.indexOf('dark') >= 0 && ((rAvg > 180) + (gAvg > 180) + (bAvg > 180) >= 2))) { // || (themeStyle.indexOf('light') >= 0 && false)
             $(img).addClass('imgInversion');
           }
           else {
@@ -126,6 +164,7 @@ class autoAdjustTheme extends api.NoteContextAwareWidget {
           }
 
         } catch (error) {
+          //console.log("Dark-mode-img-color-conversion: adjustImage error!");
           $(img).removeClass('imgInversion');
           $(img).addClass('imgNotInversion');
         }
@@ -146,12 +185,14 @@ class autoAdjustTheme extends api.NoteContextAwareWidget {
   }
 
   async entitiesReloadedEvent({ loadResults }) {
-    if (loadResults.isNoteContentReloaded(this.noteId)) {
+    await this.refresh();
+    //setTimeout("await this.refresh();", 3000 )
+    /*if (loadResults.isNoteContentReloaded(this.noteId)) {
       await this.refresh();
     } else if (loadResults.getAttributeRows().find(attr => attr.type === 'label'
       && (attr.name.toLowerCase().includes('readonly') || attr.name === 'disableAdjustImage'))) {
       await this.refresh();
-    }
+    }*/
   }
 }
 
